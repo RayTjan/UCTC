916674ccc8cd1cd15b31c8c5d488e0ee19c94746
RayTjan <62584785+RayTjan@users.noreply.github.com>
2021-01-11 21:22:44 +0700

    update

--- app/Http/Resources/Api/ProgramResource.php
+++ app/Http/Resources/Api/ProgramResource.php
@@ -17,6 +17,7 @@ class ProgramResource extends JsonResource
         return [
             'program_title'=> $this->name,
             'description'=>$this->description,
+            'goal'=>$this->goal,
             'created_by'=>$this->creator->identity->name,
             'program_date'=>$this->program_date,
             'status'=>$this->status=='1' ? 'Open' : 'Close',
